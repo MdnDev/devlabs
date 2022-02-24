8110cec0f9218bd544a84051a055abab5f3e58d2
wilfried.maudonpro@gmail.com <wilfried.maudonpro@gmail.com>
2022-02-24 23:41:56 +0100

    portfolio progress

--- frontend/src/screens/Portfolio.js
+++ frontend/src/screens/Portfolio.js
@@ -6,11 +6,11 @@ const Portfolio = () => {
     <Container>
         <h1 className='py-5'>Mes Projets</h1>
         <Row>
-            <Col>
+            <Col className="my-3" xs={12} sm={12} md={6} lg={3} xl={3}>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="../../images/jnphotography.gif" />
                     <Card.Body>
-                        <Card.Title>Jean-Noël Vireeye Photography</Card.Title>
+                        <Card.Title><h4>Jean-Noël Vireeye</h4></Card.Title>
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -19,11 +19,11 @@ const Portfolio = () => {
                 </Card>
             </Col>
 
-            <Col>
+            <Col className="my-3" xs={12} sm={12} md={6} lg={3} xl={3}>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img variant="top" src="../../images/lms.gif" />
                     <Card.Body>
-                        <Card.Title>Light Multi Services</Card.Title>
+                        <Card.Title><h4>Light Multi Services</h4></Card.Title>
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -32,11 +32,11 @@ const Portfolio = () => {
                 </Card>
             </Col>
 
-            <Col>
+            <Col className="my-3" xs={12} sm={12} md={6} lg={3} xl={3}>
                 <Card style={{ width: '18rem' }}>
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Img variant="top" src="../../images/wilfriedmaudon.gif"/>
                     <Card.Body>
-                        <Card.Title>Wilfried Maudon</Card.Title>
+                        <Card.Title><h4>Wilfried Maudon</h4></Card.Title>
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
@@ -45,11 +45,24 @@ const Portfolio = () => {
                 </Card>
             </Col>
 
-            <Col>
+            <Col className="my-3" xs={12} sm={12} md={6} lg={3} xl={3}>
                 <Card style={{ width: '18rem' }}>
                     <Card.Img variant="top" src="../../images/sorbet.gif" />
                     <Card.Body>
-                        <Card.Title>Royaume Sorbet</Card.Title>
+                        <Card.Title><h4>Royaume Sorbet</h4></Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the bulk of
+                        the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Col>
+
+            <Col className="my-3" xs={12} sm={12} md={6} lg={3} xl={3}>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="../../images/guitare.gif" />
+                    <Card.Body>
+                        <Card.Title><h4>E-commerce guitares</h4></Card.Title>
                         <Card.Text>
                         Some quick example text to build on the card title and make up the bulk of
                         the card's content.
