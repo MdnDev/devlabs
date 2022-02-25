9b2365e557f061ad430026b65768ca74a52dcf1d
wilfried.maudonpro@gmail.com <wilfried.maudonpro@gmail.com>
2022-02-25 18:27:47 +0100

    contact form added

--- frontend/src/screens/ContactScreen.js
+++ frontend/src/screens/ContactScreen.js
@@ -1,8 +1,72 @@
 import React from 'react'
+import { Container, Image, Form, Button, Row, Col } from 'react-bootstrap';
+import Message from '../components/Message';
 
 const ContactScreen = () => {
   return (
-    <div>This is the Contact Screen</div>
+    <Container >
+            <Row className="contactArtist my-5">
+              <Col style={{border: '1px solid blue'}}>
+                <Image src="../../images/logo.png" style={{height: '20rem', borderRadius: '50%'}}/>
+              </Col>
+              <Col style={{border: '1px solid red'}}>
+                <div className="my-3">
+                <h1>Wilfried Maudon</h1>
+                <h6>devlabs@outlook.fr</h6>
+                <p className="my-3" style={{textAlign: 'justify' }}>
+                  Jean-Noel Vireeye est un écrivain, cinéaste et photographe Français.
+                  Il est le fils d'une enseignante Martiniquaise qui lui permet notemment de découvrir le cinéma.
+                  Lors de ses études en histoires et en cinéma à Paris, il découvre l'art de la photographie tout en aiguisant sa plume.
+                  C'est en alliant ce rapport à l'écriture et à l'image qu'il en vient à faire ses débuts dans le cinéma avec un film primétravers le monde. 
+                  Les photos présentées sur ce site sont les premières saisies par ce jeune photographe.
+                </p>
+                </div>
+                </Col>
+             </Row>
+
+            <section >
+            <h5>Pour me contacter, veuillez remplir le formulaire ci-dessous:</h5>
+            <Form action="https://formsubmit.co/devlabs@outlook.fr" method="POST">
+                <Row>
+                    <Col>
+                        <Form.Label className="my-2">Nom</Form.Label>
+                        <Form.Control type="text" placeholder="Nom" name="Nom" />
+                    </Col>
+
+                    <Col>
+                        <Form.Label className="my-2">Prénom</Form.Label>
+                        <Form.Control type="text" placeholder="Prénom" name="Prénom" />
+                    </Col>
+                </Row>
+                
+                <Form.Label className="my-2">Email</Form.Label>
+                <Form.Control type="email" placeholder="Email" name="Email" required/>
+                
+                <Form.Label className="my-2">Objet</Form.Label>
+                <Form.Control type="text" placeholder="Objet" name="Objet" />
+
+                <label htmlFor="Message" className="my-2">
+                    Message
+                </label>
+                <textarea
+                className="form-control"
+                id="exampleFormControlTextarea1"
+                rows="5"
+                name="message"
+                />
+                <div className="field">
+                    <div data-netlify-recaptcha="true"></div>
+                </div>
+                <Button 
+                  className="my-3" 
+                  variant="dark" 
+                  type="submit" 
+                  onClick={<Message variant="success">Votre mail à été envoyé. Merci d'avoir pris contact.</Message>}>
+                    Envoyer
+                </Button>
+            </Form>
+            </section>
+        </Container>
   )
 }
 
--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -59,11 +59,51 @@ const HomeScreen = () => {
           <Row xs="12" sm="12" md="12" lg="12" xl="12">
             <h1>Ils m'ont fait confiance</h1>
             <br/>
-            <h6>Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500</h6>
           </Row>
 
           <Row className="my-3">
-           <ClientCarousel />
+           <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+           <Row>
+                    
+                    <img  className="my-4" style={{ height: '30%', width: '30%', borderRadius: '50%', margin: '10px'}} src="../../images/JN.png" alt="alt" fluid/>
+                    <h6 >Jean-Noël Vireeye, Photographe & Réalisteur</h6>
+            
+                
+            </Row>
+            <Row>
+                <p className='aboutParagraph py-4 '>
+                "Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C.".
+                </p>
+            </Row>
+           </Col>
+           <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+           <Row>
+                    
+                    <img  className="my-4" style={{ height: '30%', width: '30%', borderRadius: '50%', margin: '10px'}} src="../../images/Laurie2.jpg" alt="alt" fluid/>
+                    <h6 >Laurie Doinel, Gérante de glacerie</h6>
+            
+                
+            </Row>
+            <Row>
+                <p className='aboutParagraph py-4 '>
+                "Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C.".
+                </p>
+            </Row>
+           </Col>
+           <Col xs={12} sm={12} md={6} lg={4} xl={4}>
+           <Row>
+                    
+                    <img  className="my-4" style={{ height: '30%', width: '48%', borderRadius: '50%', margin: '10px'}} src="../../images/LMS.png" alt="alt" fluid/>
+                    <h6>Rollin Noko, Gérant, Light Multi Services</h6>
+            
+                
+            </Row>
+            <Row>
+                <p className='aboutParagraph py-4 '>
+                "Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C.".
+                </p>
+            </Row>
+           </Col>
         </Row>
 
         </Row>
