ce1cad835a4fe3b5b12ff6d0fe4647a6c26a60e0
wilfried.maudonpro@gmail.com <wilfried.maudonpro@gmail.com>
2022-02-27 01:40:00 +0100

    contact screen updated

--- frontend/src/screens/ContactScreen.js
+++ frontend/src/screens/ContactScreen.js
@@ -6,26 +6,26 @@ const ContactScreen = () => {
   return (
     <Container >
             <Row className="contactArtist my-5">
-              <Col xs={12} sm={12} md={12} lg={3} xl={3} style={{textAlign: 'center'}}>
+              <Col xs={12} sm={12} md={12} lg={4} xl={4} style={{textAlign: 'center'}}>
                 <Row>
-                  <Image className="image" src="../../images/logo.png" style={{height: '20rem', borderRadius: '50%'}}/>
+                  <Image className="image" src="../../images/logo.png" style={{height: '25rem', borderRadius: '50%'}}/>
                 </Row>
                 <Row className="mt-4">
                   <Button style={{backgroundColor: '#1d804d', fontFamily: 'Russo One, serif'}}>Github</Button>
                   </Row>
               </Col>
-              <Col xs={12} sm={12} md={12} lg={9} xl={9}>
-                <div className="my-3" style={{textAlign: 'center'}}>
+              <Col xs={12} sm={12} md={12} lg={8} xl={8}>
+                <div className="my-5" style={{textAlign: 'center'}}>
                 <h1>Wilfried Maudon</h1>
                 <h6>devlabs@outlook.fr</h6>
-                <p className="my-3" style={{textAlign: 'justify' }}>
+                <p className="my-5" style={{textAlign: 'justify' }}>
                   Jean-Noel Vireeye est un écrivain, cinéaste et photographe Français.
                   Il est le fils d'une enseignante Martiniquaise qui lui permet notemment de découvrir le cinéma.
                   Lors de ses études en histoires et en cinéma à Paris, il découvre l'art de la photographie tout en aiguisant sa plume.
                   C'est en alliant ce rapport à l'écriture et à l'image qu'il en vient à faire ses débuts dans le cinéma avec un film primétravers le monde. 
                   Les photos présentées sur ce site sont les premières saisies par ce jeune photographe.
                 </p>
-                <h3 className="py-4">Technologies que j'utilise</h3>
+                <h3 className="my-5">Technologies que j'utilise</h3>
                   <Badge pill bg="primary">
                     HTML5
                   </Badge>{' '}
@@ -54,47 +54,63 @@ const ContactScreen = () => {
               </Col>
             </Row>
 
-            <section className='py-5'>
-            <h5>Pour me contacter, veuillez remplir le formulaire ci-dessous:</h5>
-            <Form action="https://formsubmit.co/wilfried.maudonpro@gmail.com" method="POST">
-                <Row>
-                    <Col>
-                        <Form.Label className="my-2">Nom</Form.Label>
-                        <Form.Control type="text" placeholder="Nom" name="Nom" />
-                    </Col>
+            <section className='my-5'>
+              <Row>
+                <Col xs={12} sm={12} md={12} lg={4} xl={4} style={{textAlign: 'center'}}>
+                  <Image className="my-5" src="../../images/inconnus.gif" style={{height: '18rem'}}/>
+                </Col>
 
-                    <Col>
-                        <Form.Label className="my-2">Prénom</Form.Label>
-                        <Form.Control type="text" placeholder="Prénom" name="Prénom" />
-                    </Col>
-                </Row>
-                
-                <Form.Label className="my-2">Email</Form.Label>
-                <Form.Control type="email" placeholder="Email" name="Email" required/>
-                
-                <Form.Label className="my-2">Objet</Form.Label>
-                <Form.Control type="text" placeholder="Objet" name="Objet" />
+                <Col xs={12} sm={12} md={12} lg={8} xl={8} style={{textAlign: 'center'}}>
+                  <h5>Pour me contacter, veuillez remplir le formulaire ci-dessous:</h5>
 
-                <label htmlFor="Message" className="my-2">
-                    Message
-                </label>
-                <textarea
-                className="form-control"
-                id="exampleFormControlTextarea1"
-                rows="5"
-                name="message"
-                />
-                <div className="field">
-                    <div data-netlify-recaptcha="true"></div>
-                </div>
-                <Button 
-                  className="my-3" 
-                  variant="dark" 
-                  type="submit" 
-                  onClick={<Message variant="success">Votre mail à été envoyé. Merci d'avoir pris contact.</Message>}>
-                    Envoyer
-                </Button>
-            </Form>
+                  <Form action="https://formsubmit.co/wilfried.maudonpro@gmail.com" method="POST" >
+                   
+                      <Form.Label className="my-2">Nom</Form.Label>
+                      <Form.Control type="text" placeholder="Nom" name="Nom" />
+                   
+
+                    
+                      <Form.Label className="my-2">Prénom</Form.Label>
+                      <Form.Control type="text" placeholder="Prénom" name="Prénom" />
+                   
+                     
+                    
+                      <Form.Label className="my-2">Email</Form.Label>
+                      <Form.Control type="email" placeholder="Email" name="Email" required/>
+                    
+
+                    
+                      <Form.Label className="my-2">Objet</Form.Label>
+                      <Form.Control type="text" placeholder="Objet" name="Objet" />
+                    
+
+                    
+                      <label htmlFor="Message" className="my-2">
+                        Message
+                      </label>
+                      <textarea
+                      className="form-control"
+                      id="exampleFormControlTextarea1"
+                      rows="5"
+                      name="message"
+                      />
+                      <div className="field">
+                        <div data-netlify-recaptcha="true"></div>
+                      </div>
+                    
+
+                    <Button
+                      style={{backgroundColor: '#1d804d', fontFamily: 'Russo One, serif'}} 
+                      className="my-3" 
+                      variant="dark" 
+                      type="submit" 
+                      >
+                      Envoyer
+                    </Button>
+                  </Form>
+                </Col>
+              </Row>
+            
             </section>
         </Container>
   )
