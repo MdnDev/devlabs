6ef180652149e471c2c582acbc770971796142f4
wilfried.maudonpro@gmail.com <wilfried.maudonpro@gmail.com>
2022-03-02 16:43:23 +0100

    backend work started

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import blogposts from './data/blogposts.js'
+import colors from 'colors'
 
 dotenv.config()
+
+connectDB()
 const app = express()
 
 app.get('/', (req, res) => {
